d07b72dce65e5d458aadf64c3e33daf907487a46
brenowss <brenoc.fiorese@hotmail.com.br>
2020-05-25 17:50:38 -0300

    form de novo

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,21 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+function encode(data) {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+  }
 
 export default function Form() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = (evt) => {
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact-form", name, email, message })
+          })
+            .then(() => alert("Enviado com sucesso!"))
+            .catch(error => alert(error));
+
+          evt.preventDefault();
+    }
+
     return (
-        <form id="contact-form" data-netlify="true" data-netlify-honeypot="bot-field" name="contact-form" action="/">
+        <form id="contact-form" data-netlify="true" data-netlify-honeypot="bot-field" name="contact-form" onSubmit={handleSubmit}>
             <input type="hidden" name="form-name" value="contact-form" />
             <div className="form-group">
                 <label htmlFor="name">Nome:</label>
-                <input name="name" required type="text" />
+                <input name="name" required type="text" value={name} onChange={e => setName(e.target.value)} />
             </div>
             <div className="form-group">
                 <label htmlFor="email">Email:</label>
-                <input name="email" type="email" />
+                <input name="email" required type="email" value={email} onChange={e => setEmail(e.target.value)} />
             </div>
             <div className="form-group">
                 <label htmlFor="message">Mensagem:</label>
-                <textarea name="message" required rows="3" />
+                <textarea name="message" rows="3" value={message} onChange={e => setMessage(e.target.value)} />
             </div>
             <button type="submit">Enviar</button>
         </form>
